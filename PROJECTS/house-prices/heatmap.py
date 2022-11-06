79d85db544d6441351ee1688ca72af58c4b8445d
V1K1NGbg <victor@ilchev.com>
2022-11-06 22:07:55 +0000

    Nov 06 2022

--- PROJECTS/house-prices/heatmap.py
+++ PROJECTS/house-prices/heatmap.py
@@ -1,3 +1,24 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+from pyproj import Proj
 
-pd.read_csv("")
+training = pd.read_csv("PROJECTS/house-prices/HousePriceDataTRAINING.csv",names=["x", "y", "time", "price", "beds"])
+
+print(training.x)
+print(training.y)
+
+print(training.x.get(0))
+print(training.size)
+
+d = {}
+
+for i in range(training.size):
+    d[training.x.get(i)] = training.y.get(i)
+
+print(d.items())
+
+
+# training.plot.scatter(x="d.values()",y="d.keys()")
+# training.plot.scatter(x="y",y="x")
+
+plt.show()
