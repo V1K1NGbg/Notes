f88bbbc75a8b263cff06e105a32bacb9bda0eb5b
V1K1NGbg <victor@ilchev.com>
2022-11-11 12:14:29 +0000

    Nov 10 2022

--- PROJECTS/house-prices/heatmap.py
+++ PROJECTS/house-prices/heatmap.py
@@ -30,8 +30,27 @@
 import csv
 import random
 import matplotlib.pyplot as plt
-
+import math
 import pyproj
+import sys
+
+
+def formatting_data(arr):
+    minarr = sys.maxsize
+    maxarr = -sys.maxsize - 1
+    for i in range(len(arr)):
+        arr[i] = int(arr[i] * 10000000)
+        if minarr > arr[i]:
+            minarr = arr[i]
+        if maxarr < arr[i]:
+            maxarr = arr[i]
+
+    for i in range(len(arr)):
+        arr[i] = arr[i] - minarr
+    maxarr = maxarr - minarr
+
+    return arr, maxarr
+
 
 p29 = pyproj.Proj(proj='utm', zone=29, ellps='WGS84')
 p30 = pyproj.Proj(proj='utm', zone=30, ellps='WGS84')
@@ -47,27 +66,35 @@ with open('PROJECTS/house-prices/HousePriceDataTRAINING.csv') as csv_file:
     line_count = 0
     for row in csv_reader:
         # if random.randint(1, 100) <= 1:
-        #   if line_count > 10000:
-        #       break
-            x.append(float(row[1]))
-            y.append(float(row[0]))
-            line_count  = line_count + 1
+        if line_count >= 10:
+            break
+        x.append(float(row[1]))
+        y.append(float(row[0]))
+        line_count = line_count + 1
 
     print(f'Processed {line_count} lines.')
 
+# projection if time left
 
-# for i in range(len(x)):
-#     if x[i] >= -6:
-#         a, b = p30(x, y)
-#     else:
-#         a, b = p29(x, y)
-#     xcalc.append(a)
-#     ycalc.append(b)
+# print(x)
+# if x < -6:
+#     a, b = p29(x, y)
+# else:
+#     a, b = p30(x, y)
+# print(a)
+# xcalc.append(a)
+# ycalc.append(b)
 
-#cashe
+# cashe
+
+xcalc, maxx = formatting_data(x)
+ycalc, maxy = formatting_data(y)
+
+print(xcalc, ycalc, maxx, maxy)
 
 
-plt.scatter(x, y)
-#plt.scatter(xcalc, ycalc)
 
-plt.show()
+# plt.scatter(x, y)
+# #plt.scatter(xcalc, ycalc)
+
+# plt.show()
