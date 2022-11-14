46d0b718450bfdb392a866707f17e5fd64ea287e
V1K1NGbg <victor@ilchev.com>
2022-11-14 06:21:44 +0000

    Nov 14 2022

--- LABS/l07.java
+++ LABS/l07.java
@@ -2,6 +2,7 @@ import java.util.*;
 
 public class l07 {
     public static HashMap<Character, Integer> letters = new HashMap<>();
+    public static HashMap<String, Integer> map = new HashMap<>();
 
     public static int calculate(String word) {
         int sum = 0;
@@ -9,6 +10,18 @@ public class l07 {
             sum = sum + letters.get(word.toUpperCase().charAt(i));
         }
         return sum;
+    }
+
+    public static void sortmap() {
+        {
+            TreeMap<String, Integer> sorted = new TreeMap<>();
+     
+            sorted.putAll(map);
+            map.clear();
+     
+            for (Map.Entry<String, Integer> entry : sorted.entrySet())
+                map.put(entry.getKey(), entry.getValue());
+        }
     }
 
     public static void main(String[] args) {
@@ -21,29 +34,41 @@ public class l07 {
                 'V', 'W', 'Y', 'K', 'J', 'X', 'Q', 'Z' };
         int[] letterV = { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 4, 5, 8, 8, 10, 10 };
 
+        int minv = Integer.MAX_VALUE, nowv = 0;
+
+        String minw = "", noww = "";
+
         for (int i = 0; i < letterS.length; i++) {
             letters.put(letterS[i], letterV[i]);
         }
-
-        HashMap<String, Integer> map = new HashMap<>();
 
         for (int i = 0; i < words; i++) {
             word = in.next();
             map.put(word, calculate(word));
         }
-        
+
+        sortmap();
+
         for (int i = 0; i < words; i++) {
+            minv = Integer.MAX_VALUE;
+            minw = "";
 
-            int maxv = 0, nowv = Integer.parseInt(map.entrySet().toArray()[0].toString().split("=")[1]);
-            String maxw = "", noww = map.entrySet().toArray()[0].toString().split("=")[0];
+            for (int j = 0; j < map.size(); j++) {
+                nowv = Integer.parseInt(map.entrySet().toArray()[j].toString().split("=")[1]);
+                noww = map.entrySet().toArray()[j].toString().split("=")[0];
 
-            if (nowv > maxv) {
-                maxv = nowv;
-                maxw = noww;
+                if (nowv < minv) {
+                    minv = nowv;
+                    minw = noww;
+                }
             }
-            map.remove(maxw);
-            System.out.println(maxw);
+
+            map.remove(minw);
+
+            System.out.println(minw);
+
         }
+
         in.close();
     }
 }
