348958199d17886b0b768d7607754764b55445e9
V1K1NGbg <victor@ilchev.com>
2022-11-30 15:19:30 +0000

    Nov 30 2022

--- LABS/l10.java
+++ LABS/l10.java
@@ -1,26 +1,26 @@
 import java.util.*;
 
 public class l10 {
-// X,X,X, ,X,X,X,X,X,X,X, ,X,X,X,X,X,X, ,X
-// X,X,X, ,X,X,X,X,X,X,X, ,X,X,X,X,X,X, ,X
-// X,X,X, , , , , , ,X,X, ,X,X,X,X, , , ,X
-// X,X,X,X,X,X,X, ,X,X,X,X,X,X,X,X,X,X,X,X
-// X,X,X,X,X,X,X, ,X,X, , , , , , ,X,X,X,X
-// X,X,X, , ,X,X,X,X,X, ,X,X,X,X, ,X,X,X,X
-// X,X, , ,X, ,X,X,X,X, , , ,X,X, ,X,X,X,X
-// X,X,X, ,X,X,X,X,X,X,X,X, ,X,X, ,X,X,X,X
-// X,X, , ,X, , ,X,X,X,X,X, ,X,X, ,X,X,X,X
-// X,X,X,X,X,X, , , , , , , ,X,X, ,X,X,X,X
-// X, ,X,X,X,X, ,X,X, ,O,X,X,X,X, ,X,X,X,X
-// , , , , ,X,X,X,X, , ,X,X,X,X, ,X,X,X,X
-// X,X,X,X,X,X,X,X,X,X,X,X,X,X,X, ,X,X,X,X
-// X,X,X,X,X,X, , ,X,X,X,X, , , , ,X,X,X,X
-// X,X, ,X,X, ,X,X,X,X,X,X, ,X,X, ,X,X,X,X
-// X, , ,X,X, ,X,X,X,X,X,X, ,X,X, ,X,X,X,X
-// X,X, ,X,X, ,X, , ,X, , , ,X,X, ,X,X, ,
-// X, , ,X,X,X,X,X,X,X, ,X,X,X,X, ,X,X, ,X
-// X,X, ,X,X,X,X,X,X,X, ,X,X,X,X,X,X,X, ,X
-// X,X, ,X,X,X,X,X,X,X, ,X,X,X,X,X,X,X, ,X
+    // X,X,X, ,X,X,X,X,X,X,X, ,X,X,X,X,X,X, ,X
+    // X,X,X, ,X,X,X,X,X,X,X, ,X,X,X,X,X,X, ,X
+    // X,X,X, , , , , , ,X,X, ,X,X,X,X, , , ,X
+    // X,X,X,X,X,X,X, ,X,X,X,X,X,X,X,X,X,X,X,X
+    // X,X,X,X,X,X,X, ,X,X, , , , , , ,X,X,X,X
+    // X,X,X, , ,X,X,X,X,X, ,X,X,X,X, ,X,X,X,X
+    // X,X, , ,X, ,X,X,X,X, , , ,X,X, ,X,X,X,X
+    // X,X,X, ,X,X,X,X,X,X,X,X, ,X,X, ,X,X,X,X
+    // X,X, , ,X, , ,X,X,X,X,X, ,X,X, ,X,X,X,X
+    // X,X,X,X,X,X, , , , , , , ,X,X, ,X,X,X,X
+    // X, ,X,X,X,X, ,X,X, ,O,X,X,X,X, ,X,X,X,X
+    // , , , , ,X,X,X,X, , ,X,X,X,X, ,X,X,X,X
+    // X,X,X,X,X,X,X,X,X,X,X,X,X,X,X, ,X,X,X,X
+    // X,X,X,X,X,X, , ,X,X,X,X, , , , ,X,X,X,X
+    // X,X, ,X,X, ,X,X,X,X,X,X, ,X,X, ,X,X,X,X
+    // X, , ,X,X, ,X,X,X,X,X,X, ,X,X, ,X,X,X,X
+    // X,X, ,X,X, ,X, , ,X, , , ,X,X, ,X,X, ,
+    // X, , ,X,X,X,X,X,X,X, ,X,X,X,X, ,X,X, ,X
+    // X,X, ,X,X,X,X,X,X,X, ,X,X,X,X,X,X,X, ,X
+    // X,X, ,X,X,X,X,X,X,X, ,X,X,X,X,X,X,X, ,X
     public static void main(String[] args) {
 
         int lives = 200;
@@ -71,7 +71,8 @@ public class l10 {
             String move = myBrain.getMove(maze[posX - 1][posY], maze[posX + 1][posY], maze[posX][posY + 1],
                     maze[posX][posY - 1]);
 
-            System.out.println(move);
+            System.out.println(move + maze[posX - 1][posY] + maze[posX + 1][posY] + maze[posX][posY + 1]
+                    + maze[posX][posY - 1] + posX + posY);
 
             if (move.equals("north") && maze[posX - 1][posY]) {
 
@@ -138,9 +139,34 @@ class Brain {
         }
 
     }
-    
-    public String move(boolean north, boolean south, boolean west, boolean east) {
 
+    public String direction(boolean primary, boolean secondary, boolean tertiary, int elseint, int primaryint,
+            int secondaryint,
+            int tertiaryint) {
+        if (primary) {
+            return (singlemove(primaryint));
+        } else if (secondary) {
+            return (singlemove(secondaryint));
+        } else if (tertiary) {
+            return (singlemove(tertiaryint));
+        } else {
+            return (singlemove(elseint));
+        }
+    }
+
+    public String move(boolean north, boolean south, boolean west, boolean east) {
+        switch (last) {
+            case 0:
+                return (direction(north, east, west, 1, 0, 3, 2));
+            case 1:
+                return (direction(south, west, east, 0, 1, 2, 3));
+            case 2:
+                return (direction(west, north, south, 3, 2, 0, 1));
+            case 3:
+                return (direction(east, south, north, 2, 3, 1, 0));
+            default:
+                return ("Error");
+        }
     }
 
     public String getMove(boolean north, boolean south, boolean east, boolean west) {
@@ -162,29 +188,31 @@ class Brain {
             count = count - 1;
         if (points[x][y - 1])
             count = count - 1;
-        switch (count) {
-            case 1:
-                points[x][y] = true;
-                return (move(north, south, west, east));
-            case 2:
-                return (move(north, south, west, east));
-            case 3:
-                return (move(north, south, west, east));
-            case 4:
-                return (move(north, south, west, east));
-            case 0:
-                points[x][y] = true;
-                switch (stack.pop()) {
-                    case 0:
-                        return ("south");
-                    case 1:
-                        return ("north");
-                    case 2:
-                        return ("east");
-                    case 3:
-                        return ("west");
-                }
+
+        points[x][y] = true;
+        if (count == 0) {
+            switch (stack.pop()) {
+                case 1:
+                    last = 0;
+                    y = y - 1;
+                    return ("north");
+                case 0:
+                    last = 1;
+                    y = y + 1;
+                    return ("south");
+                case 3:
+                    last = 2;
+                    x = x - 1;
+                    return ("west");
+                case 2:
+                    last = 3;
+                    x = x + 1;
+                    return ("east");
+                default:
+                    return ("Error");
+            }
+        } else {
+            return (move(north, south, west, east));
         }
-        return ("Error");
     }
 }
